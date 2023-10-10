6adc13818e99c008f414f95ae5625d7248e289f5
nkbpp <alexbratchin@gmail.com>
2023-10-10 09:51:46 +0300

    Initial commit

---
+++ src/main/java/ru/bratchin/coursework1sb/Coursework1sbApplication.java
@@ -0,0 +1,13 @@
+package ru.bratchin.coursework1sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework1sbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework1sbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bratchin/coursework1sb/Coursework1sbApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.bratchin.coursework1sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework1sbApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Coursework1sbApplication.main(new String[]{});
+    }
+
+}
